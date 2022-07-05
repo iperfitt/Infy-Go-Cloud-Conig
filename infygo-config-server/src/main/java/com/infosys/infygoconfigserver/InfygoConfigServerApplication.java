30ae74a5d6775cd058d55c62cceb782e0119b813
iperfitt <iperfitt@umich.edu>
2022-07-04 22:01:07 -0500

    Add files via upload

---
+++ infygo-config-server/src/main/java/com/infosys/infygoconfigserver/InfygoConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.infosys.infygoconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfygoConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfygoConfigServerApplication.class, args);
+	}
+
+}
